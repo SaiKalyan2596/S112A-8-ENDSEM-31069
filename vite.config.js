5fa45e7de737f6737f414793b493ef3383d8ad98
SaiKalyan2596 <anakinskywalker11021@gmail.com>
2025-11-17 15:22:24 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hospital/',
+  base: '/S112A-8-ENDSEM-31069/',
   plugins: [react()],
 })
